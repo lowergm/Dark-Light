8d4cc98245303f16f86fb99f96fc483e8ef66219
lowergm <92257300+lowergm@users.noreply.github.com>
2025-06-25 15:02:30 -0300

    Create pages.php
    Criar as páginas que serão validadas no index.php

---
+++ public/pages/pages.php
@@ -0,0 +1,20 @@
+<?php
+
+$paginas = ["home"=>"", "sobre"=>"", "licensa"=>""];
+
+$paginas["home"] = "
+<h2>Bem vindo!</h2>
+<p>Programando o futuro!</p>
+";
+
+$paginas["sobre"] = "
+<h2>Sobre</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eros enim, rhoncus in fringilla in, volutpat eu tortor. Nulla nec velit arcu. Sed porta laoreet purus, id commodo urna dignissim nec. Donec vulputate rhoncus tristique. Donec mattis, magna vel sollicitudin dignissim, erat odio gravida nibh, et tempor libero ligula ac risus. Sed consequat facilisis iaculis. Suspendisse quam lectus, ornare et nulla quis, cursus lacinia diam. Phasellus varius magna a augue mollis, scelerisque ultrices felis accumsan. Duis viverra nibh sit amet tincidunt vestibulum. Maecenas egestas sollicitudin erat</p>
+";
+
+$paginas["licensa"] = "
+<h2>Lincensa CR7</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eros enim, rhoncus in fringilla in, volutpat eu tortor. Nulla nec velit arcu. Sed porta laoreet purus, id commodo urna dignissim nec. Donec vulputate rhoncus tristique. Donec mattis, magna vel sollicitudin dignissim, erat odio gravida nibh, et tempor libero ligula ac risus. Sed consequat facilisis iaculis. Suspendisse quam lectus, ornare et nulla quis, cursus lacinia diam. Phasellus varius magna a augue mollis, scelerisque ultrices felis accumsan. Duis viverra nibh sit amet tincidunt vestibulum. Maecenas egestas sollicitudin erat</p>
+";
+
+?>
