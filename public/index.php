597ff31886efa504c520327a29f33d83f7e2b10d
lowergm <92257300+lowergm@users.noreply.github.com>
2025-06-25 15:01:19 -0300

    Create index.php
    Validar as páginas e garantir a segurança do código e do site

--- public/index.php
+++ public/index.php
@@ -1,17 +1,40 @@
-<!doctype html>
+<?php
+include "pages/pages.php";
+
+?>
+
+<!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8"/>
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-    <div class="div-main">
-        <h1>Bem Vindo!</h1>
-        <p>
-            <?php
-                echo "Este é meu primeiro Site em PHP.";
-            ?>
-        </p>
-    </div>
-  </body>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="static/css/style.css" />
+    <title>Validation System</title>
+</head>
+<body>
+    <header>
+    <?php
+    
+    foreach($paginas as $key => $value){
+        echo "<a href='?page=$key'>".ucfirst($key)."</a>";
+    }
+    
+    ?>
+    <button id="tema">Trocar tema</button>
+    </header>
+    <section>
+    <?php
+    
+    $pagina = (isset($_GET["page"]) ? $_GET["page"] : "home");
+    
+    if(!array_key_exists($pagina, $paginas)){
+        $pagina = "home";
+    }
+    
+    echo $paginas[$pagina];
+    
+    ?>
+    </section>
+    <script src="static/js/tema.js"></script>
+</body>
 </html>
