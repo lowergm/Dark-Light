7e17695146f7521db6154d6f8c0002ff6ee82134
lowergm <92257300+lowergm@users.noreply.github.com>
2025-06-25 17:07:33 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ include "pages/pages.php";
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="pt-BR">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
